36fcc1a8516db7881b8f83a0e8570b57d309d7a0
chewjrr <125365241+chewjrr@users.noreply.github.com>
2023-04-07 23:50:29 +0300

    Create 8prac_digital_sensor.cpp

---
+++ prac8/8prac_digital_sensor.cpp
@@ -0,0 +1,33 @@
+﻿//реализация цифрового датчика на примера датчика наполнения контейнира с готовыми изделиями (I2)
+//контейнер максимум вмещает 100 заготовок, диапозон значений от 0 до 1
+
+#include <iostream>
+
+class digital_sensor {
+private:
+	float value;
+public:
+	void Get_value(float x) {
+		value = x / 100;
+	}
+	void Show_value() {
+		std::cout << "Датчик выдал значение: " << value << std::endl << std::endl;
+	}
+};
+
+int main() {
+	digital_sensor i2;
+
+	setlocale(LC_ALL, "Russian");
+
+	float a = 0;
+
+	std::cout << "Введите количество заготовок в контейнере (макс. 100): ";
+	std::cin >> a;
+	std::cout << std::endl;
+
+	i2.Get_value(a);
+	i2.Show_value();
+
+	return 0;
+}
